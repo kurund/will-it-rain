9f259740ca5b0f0b7f51a0239c146c41b8a20f67
Matt Hall <github@matthall.tv>
2025-10-04 12:33:35 +0100

    checks for valid date

--- api/main.py
+++ api/main.py
@@ -1,19 +1,29 @@
+import datetime
 import json
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return json.dumps({"message": "Weather API"})
 
 
 @app.get("/weather")
 def get_weather(location: Union[str, None] = None, date: Union[str, None] = None):
+    if not location or not date:
+        raise HTTPException(status_code=400, detail="Bad request")
+
+    try:
+        d = datetime.date.fromisoformat(date)
+    except:
+        raise HTTPException(status_code=400, detail="Bad date")
+
     data = {
+        "date": d.isoformat(),
         "precipitation": 4,
         "temperature": 17,
     }
