c8d4cf10521e2553204cb84546ec2c721f232cf9
Matt Hall <github@matthall.tv>
2025-10-04 15:12:35 +0100

    added CORS

--- api/main.py
+++ api/main.py
@@ -4,8 +4,21 @@ import random
 from typing import Union
 
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
