52996705cd1e4f35c25155f87d5a9e6ef5918ca5
Kurund Jalmi <kurund.jalmi@gmail.com>
2025-10-05 21:00:55 +0100

    update favicon

--- webapp/src/routes/+layout.svelte
+++ webapp/src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
 	import '../app.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import Footer from '$lib/components/Footer.svelte';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
 
 <div class="flex min-h-screen flex-col">
 	<main class="flex-1">
